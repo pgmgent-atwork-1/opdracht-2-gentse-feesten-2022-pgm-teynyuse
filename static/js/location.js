0b49b148b60e890babaab4b6505b9646213d6039
«pgm-teynyuse» <teynur.yuseinov@student.arteveldehs.be>
2023-08-03 20:20:11 +0200

    raster-list

--- static/js/location.js
+++ static/js/location.js
@@ -1,79 +1,44 @@
-const categories_URL = "https://www.pgm.gent/data/gentsefeesten/categories.json";
+const locations_URL = 'https://www.pgm.gent/data/gentsefeesten/events_500.json';
+
 
 (() => {
     const app = {
         init() {
             this.cacheElements();
-            this.fetchData();
-            this.toggleList();
-            this.toggleRaster();
+            this.fetchLocationsForEvents();
+            this.fetchEventsForLocations();
         },
 
         cacheElements() {
-            this.$btnDay = document.querySelector('#change-day');
-            this.$asideDay = document.querySelector('#aside-day');
-            this.$crossDay = document.querySelector('#cross-day');
-            this.$event = document.querySelector("#event");
-            this.$detail = document.querySelector("#detail");
-            this.$filterCategory = document.querySelector("#filter-items");
-            this.$filterLocations = document.querySelector("#filter-locations");
-            this.$toggleList = document.querySelector("#toggle-list");
-            this.$toggleRaster = document.querySelector("#toggle-raster");
+            this.$locations = document.querySelector('#locations');
+            this.$locationsEvent = document.querySelector('#eventLocations');
+        },
+        fetchLocationsForEvents() {
+            fetch(locations_URL)
+                .then((response) => response.json())
+                .then((locations) => {
+                    if(this.$locations) {
+                        this.$locations.innerHTML = this.renderHTMLForLocations(locations);
+                        console.log(locations)
+                    }
+                })
+                .catch((error) => console.log(error));
+        },
+        fetchEventsForLocations() {
+            fetch(locations_URL)
+                .then((response) => response.json())
+                .then((locationsEvent) => {
+                    if(this.$eventLocations) {
+                        this.$eventLocations.innerHTML = this.renderHTMLEventForLocations(locationsEvent);
+                        console.log(locationsEvent)
+                    }
+                })
+                .catch((error) => console.log(error));
         },
 
-        toggleRaster() {
-            this.$toggleRaster.addEventListener("click", (e) => {
-                this.$event.classList.remove("event-list");
-                this.$event.classList.add("raster-event");
-
-                this.$toggleList.classList.remove("active");
-                this.$toggleRaster.classList.add("active");
-            })
-        },
-
-        toggleList() {
-            this.$toggleList.addEventListener("click", (e) => {
-                this.$event.classList.remove("raster-event");
-                this.$event.classList.add("event-list");
-
-                this.$toggleRaster.classList.remove("active");
-                this.$toggleList.classList.add("active");
-            })
-        },
-
-        async fetchCategory() {
-            let response = await fetch(categories_URL);
-            return await response.json();
-        },
-        async fetchLocation() {
-            let response = await fetch(events_URL);
-            return await response.json();
-        },
-        async fetchEvents() {
-            let response = await fetch(events_URL);
-            return await response.json();
-        },
-
-        async fetchData() {
-            try {
-                let categories = await this.fetchCategory();
-                const events = await this.fetchEvents();
-                const locations = await this.fetchLocation();
-
-                if (this.$event) {
-                this.$event.innerHTML = this.renderHTMLForCategory(categories,events,locations);
-                }
-
-                this.$filterCategory.innerHTML = this.renderHTMLForFilterCategory(categories);
-                this.$filterLocations.innerHTML = this.renderHTMLForFilterLocation(locations);
-            } catch (error) {
-                console.log(error);
-            }
-        },
-
-        renderHTMLForFilterCategory(categories) {
-            return categories
-                .map((category) => {
+        renderHTMLForLocations(locations) {
+            return locations
+                .map((locations) => {
                 return `
                         <li class="category_name">
                         <?xml version="1.0" encoding="utf-8"?>
@@ -86,91 +51,25 @@ const categories_URL = "https://www.pgm.gent/data/gentsefeesten/categories.json"
                         s-0.4-2.2-1.2-3c-1.6-1.6-4.4-1.6-6,0l-13,13c-1.3,1.3-2.1,3.1-2.1,5s0.7,3.7,2.1,5c1.3,1.3,3.1,2.1,5,2.1s3.7-0.7,5-2.1l13-13
                         c0.6-0.6,1.4-0.6,2,0s0.6,1.4,0,2l-13,13C15.9,31,13.4,32,10.7,32L10.7,32z"/>
                     </svg>
-                        <a href="#${category}">
-                            <p>${category}</p>
+                        <a href="#${locations.location}">
+                            <p>${locations.location}</p>
                         </a>
                     </li>`;
             }).join("\n");
         },
-        renderHTMLForFilterLocation(locations) {
-            const uniqueLocations = locations.filter((event, index, self) => {
-                return index === self.findIndex((e) => e.location === event.location);
-            });
-
-            return uniqueLocations
-                .map((event) => {
-                    return `
-                        <li class="location_name">
-                            <a href="#${event.location}">
-                                <p>${event.location}</p>
-                            </a>
-                        </li>`;
-                }).join("\n");
-        },
-
-        renderHTMLForCategory(categories, events) {
-            const params = new URLSearchParams(window.location.search);
-            const day = params.get("day") ?? "15";
-            return categories
-                .map((category) => {
-                let filteredEvents = events.filter((event) => {
-                    return event.day === day && event.category.includes(category);
-                });
-                console.log(filteredEvents);
-                return `
-                    <div>
-                        <div class="category">
-                            <h2 id="${category}">${category}</h2>
-                            <a href="#filter-items">
-                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="18" height="32" viewBox="0 0 18 32">
-                                    <title>arrow-up</title>
-                                    <path d="M17.809 9.9l-8.88-9.9-8.929 9.897 2.225 2.007 5.189-5.752-0 25.848h2.997l0-25.863 5.169 5.763z"></path>
-                                </svg>
-                            </a>
-                        </div>
-                        <ul class="items-category">
-                            ${filteredEvents
-                                .map((event) => {
-                                return `
-                                    <li class="items-category">
-                                        <a href="detail.html?slug=${event.slug}&day=${event.day}" class="event-card">
-                                            <div class="div-event">
-                                                <div class="event-date">
-                                                    <p>${event.day_of_week} ${event.day} JULI</p>
-                                                    <div class="event-img">
-                                                        ${this.pictureEvent(event)}
-                                                    </div>
-                                                </div>
-                                                <div class="event-info">
-                                                    <p class="event-title">${event.title}</p>
-                                                    <div class="event-location">
-                                                        <span>${event.location}</span>
-                                                        <p class="event-start">${event.start} u.</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </li>`;
-                            }).join("")}
-                        </ul>
-                    </div>
-                    `;
-            })
-            .join("");
-        },
-        renderHTMLForLocations(locations, events) {
+        renderHTMLForLocationsEvents(locations) {
             const params = new URLSearchParams(window.location.search);
             const day = params.get("day") ?? "15";
             return locations
-                .map((category) => {
-                let filteredEvents = events.filter((event) => {
-                    return event.day === day && event.category.includes(category);
+                .map((location) => {
+                let filteredLocations = events.filter((event) => {
+                    return event.day === day && event.location.includes(location);
                 });
-                console.log(filteredEvents);
+                console.log(filteredLocations);
                 return `
                     <div>
-                        <div class="location">
-                            <h2 id="${location}">${location}</h2>
+                        <div class="category">
+                            <h2 id="${locations.location}">${locations.location}</h2>
                             <a href="#filter-items">
                                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="18" height="32" viewBox="0 0 18 32">
                                     <title>arrow-up</title>
@@ -184,7 +83,7 @@ const categories_URL = "https://www.pgm.gent/data/gentsefeesten/categories.json"
                                 return `
                                     <li class="items-category">
                                         <a href="detail.html?slug=${event.slug}&day=${event.day}" class="event-card">
-                                            <div class="div-event">
+                                            <div class="event-box">
                                                 <div class="event-date">
                                                     <p>${event.day_of_week} ${event.day} JULI</p>
                                                     <div class="event-img">
@@ -211,11 +110,11 @@ const categories_URL = "https://www.pgm.gent/data/gentsefeesten/categories.json"
 
         pictureEvent(events) {
             if (events.image === null) {
-                return `<img src="../static/img/no-photo.jpg">`;
+                return `<img src="static/img/no-photo.jpg">`
             } else {
-                return `<img src="${events.image.full}" alt="${events.title}">`;
+                return `<img src="${events.image.full}" alt="${events.title}">`
             }
         },
-    };
+    }
     app.init();
 })();
